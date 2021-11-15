d31acc0d79a3c55c7878ac151ed5a6dba00690cc
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2021-11-15 13:33:37 -0500

    Update control_structures.js

--- scripts/control_structures.js
+++ scripts/control_structures.js
@@ -1,1 +1,20 @@
-
+import { World, Commands } from "mojang-minecraft";
+let loaded = false;
+World.events.tick.subscribe(() => {
+  try {
+    const dimension = World.getDimension('overworld');
+    if (!loaded) {
+      Commands.run('scoreboard objectives add Tick dummy', dimension);
+      Commands.run('scoreboard players add Dummy Tick 0', dimension);
+      Commands.run('scoreboard objectives add TickTwo dummy', dimension);
+      Commands.run('scoreboard players add Dummy TickTwo 0', dimension);
+      loaded = true
+    } const tick = parseInt(Commands.run('scoreboard players test Dummy Tick *', dimension).statusMessage.match(/-?\d+/));
+    if (tick) {
+      Commands.run('tittle @a actionbar Hello World!', dimension);
+    } 
+    
+  } catch (error) {
+      conesole.warn(error, error.stack)
+  }
+});
