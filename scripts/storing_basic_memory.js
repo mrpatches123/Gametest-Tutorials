836f93dc250c28c4303e935a97b81f8f76d82075
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2021-11-15 11:39:07 -0500

    Update storing_basic_memory.js

--- scripts/storing_basic_memory.js
+++ scripts/storing_basic_memory.js
@@ -1,1 +1,14 @@
+import { World, Commands } from "mojang-minecraft";
 
+World.events.tick.subscribe(() => {
+  try {
+    try {
+      Commands.run('scoreboard players set @s CommandTest 2');
+    } catch {
+      Commands.run('scoreboard objectives add CommandTest dummy');
+      Commands.run('scoreboard players set @s CommandTest 2');
+    }
+  } catch (error) {
+      conesole.warn(error, error.stack)
+  }
+});
