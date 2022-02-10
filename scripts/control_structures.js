1a378cf3e7af7fb04516082029fc6e5c88ff6a6d
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2022-02-10 13:37:39 -0500

    1.18.10

--- scripts/control_structures.js
+++ scripts/control_structures.js
@@ -1,39 +1,60 @@
-import { World, Commands } from "mojang-minecraft";
+import { world } from "mojang-minecraft";
 let loaded = false;
-const dimension = World.getDimension('overworld');
-World.events.tick.subscribe(() => {
+const overworld = World.getDimension('overworld');
+let joiningPlayers = [];
+world.events.playerJoin.subscribe(({player}) => {
+  joiningPlayers.unshift(player);
+})
+
+function initaliseServer() {
+  overworld.runCommand('scoreboard objectives add Tick dummy');
+  overworld.runCommand('scoreboard players add Dummy Tick 0');
+  overworld.runCommand('scoreboard objectives add TickTwo dummy');
+  overworld.runCommand('scoreboard players add Dummy TickTwo 0');
+  overworld.runCommand('scoreboard objectives add DisplaySetting dummy');
+  overworld.runCommand('scoreboard players add Dummy DisplaySetting 0');
+}
+
+function intailisePlayer(player) {
+  
+}
+world.events.tick.subscribe(() => {
   try {
-    if (!loaded) {
-      Commands.run('scoreboard objectives add Tick dummy', dimension);
-      Commands.run('scoreboard players add Dummy Tick 0', dimension);
-      Commands.run('scoreboard objectives add TickTwo dummy', dimension);
-      Commands.run('scoreboard players add Dummy TickTwo 0', dimension);
-      Commands.run('scoreboard objectives add DisplaySetting dummy', dimension);
-      Commands.run('scoreboard players add Dummy DisplaySetting 0', dimension);
-      loaded = true
-    } const tick = parseInt(Commands.run('scoreboard players test Dummy Tick *', dimension).statusMessage.match(/-?\d+/));
+    joiningPlayers.forEach(player => {
+      try {
+        player.runCommand('testfor @s');
+        joiningPlayers = joiningPlayers.filter(joiningPlayer => player.nameTag !== joiningPlayer.nameTag);
+        intailisePlayer(player);
+        if (!loaded) { 
+          initaliseServer() 
+          loaded = true
+        }
+        
+      } catch { }
+      
+    });
+ const tick = parseInt(overworld.runCommand('scoreboard players test Dummy Tick *').statusMessage.match(/-?\d+/));
     if (tick) {
-      Commands.run('tittle @a actionbar Hello World!', dimension);
+      overworld.runCommand('tittle @a actionbar Hello World!');
     }
   } catch (error) {
-      conesole.warn(error, error.stack)
+      conesole.warn(error, error.stack);
   }
 });
 
-World.events.tick.subscribe(msg => {
+World.events.tick.subscribe(({message, sender}) => {
   try {
-    message = msg.message;
     switch (message) {
       case 'hello':
       case 'hi':
-        Commands.run('say Hello, Welcome to the world', dimension);
+        overworld.runCommand('say Hello, Welcome to the world');
         break;
       case 'help':
-        Commands.run('read the "how to play" book in spawn', dimension);
+        overworld.runCommand('read the "how to play" book in spawn');
         break;
       case 'time':
-        const date = new Date().toString()
-        Commands.run(`${date}`, dimension);
+        const date = (new Date()).toString();
+        overworld.runCommand(`${date}`);
         break;
       default:
         break;
