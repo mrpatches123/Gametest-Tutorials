1083bb548d1440584ca8df7754f4951dca7b6220
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2022-02-11 12:19:19 -0500

    Update player_iteration.js

--- scripts/player_iteration.js
+++ scripts/player_iteration.js
@@ -1,6 +1,7 @@
 import { world } from "mojang-minecraft";
 
 const overworld = World.getDimension('overworld');
+const {floor, hypot} = Math
 
 let loaded = false;
 let joiningPlayers = [];
@@ -15,6 +16,21 @@ function initaliseServer() {
 function intailisePlayer(player) {
   
 }
+
+//key: # = to its topSoild or in a 'from' it doesn't check that axis, ~ = reletive (commands like), ~* = center relitive, 773 = number
+//dimesion defalut for the teleport will be overword
+const teleports = [
+  {
+    name:'spawn',
+    from: [0, '#', 0],
+    to: [4,'#',80]
+  },
+  {
+    name:'place',
+    from: [3, '#', 3],
+    to: ['~5','~10','~5']
+  }
+];
 world.events.tick.subscribe(() => {
   try {
     joiningPlayers.forEach(player => {
@@ -31,8 +47,12 @@ world.events.tick.subscribe(() => {
     });
     let players = world.getPlayers().filter(player => !joiningPlayers.some(join => join.nameTag === player.nameTag));
     for (let player of players) {
-      const {name, location,velocity} = player
-      console.warn
+      const {name, location, velocity} = player;
+      const {x,y,z} = location
+      const {x: xv,y: yv,z: zv} = velocity
+      player.runCommand(`title @s ${name}, ${floor(location.x)}, ${floor(y)}, ${floor(z)}, ${floor(x)}, ${hypot(xv,yv,zv)}`)
+      teleports.forEach({name,to,form})
+      
     }
   } catch (error) {
       console.warn(error, error.stack);
