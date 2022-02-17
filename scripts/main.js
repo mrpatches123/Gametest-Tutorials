ee60bf1dc7e65dbe1a5f7587e471cd3958decc1d
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2022-02-17 11:04:35 -0500

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 //import "./player_iteration.js"
-import {world} from 'mojang_minecraft'
+import {world} from 'mojang-minecraft'
 
 world.events.entityHit.subscribe(({entity,hitBlock,hitEntity}) => {
-  console.warn(entity.id,(hitEntity ?? {}).id,((hitBlock ?? {}).type ?? {}).id)
+  try {
+    console.warn(entity.id,(hitEntity ?? {}).id,((hitBlock ?? {}).type ?? {}).id)
+  } catch (error) {
+    console.warn(error, error.stack)
+  }
 })
